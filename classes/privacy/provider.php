82a55e26fe4239ae4adaacdf06410fd60684ffd0
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2021-02-25 05:00:20 +1100

    Add null privacy provider

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
- * Plugin strings are defined here.
+ * Privacy Subsystem implementation for tool_token.
  *
  * @package     tool_token
  * @category    string
@@ -23,8 +23,25 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+namespace tool_token\privacy;
+
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'Token generator';
-$string['token:generatetoken'] = 'Generate Token';
-$string['privacy:metadata'] = 'The Token generator plugin does not store any personal data.';
+/**
+ * Privacy Subsystem for tool_task implementing null_provider.
+ *
+ * @copyright   2021 Catalyst IT
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\local\metadata\null_provider {
+
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function get_reason() : string {
+        return 'privacy:metadata';
+    }
+}
