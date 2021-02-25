639ac57e3c007578bc7321ca50cb63ee64551d8a
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2021-02-25 12:24:38 +1100

    Add class to extract users by provided field

---
+++ classes/user_extractor.php
@@ -0,0 +1,98 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The class responsible for retrieving a user based on identifier.
+ *
+ * @package     tool_token
+ * @copyright   2021 Catalyst IT
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_token;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The class responsible for retrieving a user based on identifier.
+ *
+ * @copyright   2021 Catalyst IT
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class user_extractor {
+
+    /** An instance of fields config class.
+     * @var \tool_token\fields_config
+     */
+    protected $fieldscofnig;
+
+    /**
+     * Constructor.
+     *
+     * @param \tool_token\fields_config $fieldsconfig An instance of fields config class.
+     */
+    public function __construct(fields_config $fieldsconfig) {
+        $this->fieldscofnig = $fieldsconfig;
+    }
+
+    /**
+     * Get extracted from DB user.
+     *
+     * @param string $fieldname Field name to search by.
+     * @param string $fieldvalue Field value to search by.
+     *
+     * @return \stdClass|null
+     */
+    public function get_user(string $fieldname, string $fieldvalue) : ?\stdClass {
+        global $DB;
+
+        if (!$this->fieldscofnig->is_field_enabled($fieldname)) {
+            throw new incorrect_field_exception('Field "' . $fieldname . '" is not enabled for fetching users');
+        }
+
+        $user = null;
+
+        $params = [];
+        $joins = "";
+        $where = "";
+
+        if ($this->fieldscofnig->is_custom_profile_field($fieldname)) {
+            $joins .= " LEFT JOIN {user_info_field} f ON f.shortname = :fieldname ";
+            $joins .= " LEFT JOIN {user_info_data} d ON d.fieldid = f.id AND d.userid = u.id ";
+            $where .= "AND d.data = :fieldvalue ";
+            $params['fieldname'] = $fieldname;
+            $params['fieldvalue'] = $fieldvalue;
+        } else {
+            $caseinsensitive = ($fieldname == 'id');
+            $fieldselect = $DB->sql_equal($fieldname, ':fieldvalue', $caseinsensitive);
+
+            $where .= "AND $fieldselect ";
+            $params['fieldvalue'] = $fieldvalue;
+        }
+
+        $sql = "SELECT u.* FROM {user} u $joins WHERE u.deleted=0 $where";
+
+        if ($result = $DB->get_records_sql($sql, $params)) {
+            if (count($result) > 1) {
+                throw new more_than_one_user_exception('More than one user found.');
+            }
+            $user = reset($result);
+            profile_load_custom_fields($user);
+        }
+
+        return $user;
+    }
+}
---
+++ tests/user_extractor_test.php
@@ -0,0 +1,186 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for user_extractor class.
+ *
+ * @package     tool_token
+ * @copyright   2021 Catalyst IT
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use tool_token\user_extractor;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+class tool_token_user_extractor_testcase extends advanced_testcase {
+
+    /**
+     * Mocked field config instance.
+     * @var \tool_token\fields_config
+     */
+    protected $fieldsconfig;
+
+    /**
+     * Set up.
+     */
+    public function setUp() {
+        parent::setUp();
+        $builder = $this->getMockBuilder('\tool_token\fields_config')
+            ->setMethods(['is_field_enabled', 'is_custom_profile_field']);
+        $this->fieldsconfig = $builder->getMock();
+    }
+
+    /**
+     * A helper function to create a custom profile field.
+     *
+     * @param string $shortname Short name of the field.
+     * @param string $datatype Type of the field, e.g. text, checkbox, datetime, menu and etc.
+     * @param bool $unique Should the field to be unique?
+     *
+     * @return \stdClass
+     */
+    protected function add_user_profile_field(string $shortname, string $datatype, bool $unique = false) : stdClass {
+        global $DB;
+
+        // Create a new profile field.
+        $data = new \stdClass();
+        $data->shortname = $shortname;
+        $data->datatype = $datatype;
+        $data->name = 'Test ' . $shortname;
+        $data->description = 'This is a test field';
+        $data->required = false;
+        $data->locked = false;
+        $data->forceunique = $unique;
+        $data->signup = false;
+        $data->visible = '0';
+        $data->categoryid = '0';
+
+        $DB->insert_record('user_info_field', $data);
+
+        return $data;
+    }
+
+    /**
+     * Test a correct exception is thrown when trying to search by disable field.
+     */
+    public function test_get_user_throw_exception_on_not_enabled_field() {
+        $this->fieldsconfig->method('is_field_enabled')->willReturn(false);
+        $this->fieldsconfig->method('is_custom_profile_field')->willReturn(false);
+
+        $extractor = new user_extractor($this->fieldsconfig);
+
+        $this->expectException('tool_token\incorrect_field_exception');
+        $this->expectExceptionMessage('Field "disabled" is not enabled for fetching users');
+
+        $extractor->get_user('disabled', 'test');
+    }
+
+    /**
+     * Test a correct exception is thrown when trying to search by disable field.
+     */
+    public function test_get_user_throw_exception_if_found_more_than_one_user() {
+        $this->resetAfterTest();
+
+        $this->fieldsconfig->method('is_field_enabled')->willReturn(true);
+        $this->fieldsconfig->method('is_custom_profile_field')->willReturn(false);
+
+        // Two users with empty idnumber.
+        $user1 = $this->getDataGenerator()->create_user();
+        $user2 = $this->getDataGenerator()->create_user();
+
+        $extractor = new user_extractor($this->fieldsconfig);
+
+        $this->expectException('tool_token\more_than_one_user_exception');
+        $this->expectExceptionMessage('More than one user found.');
+
+        $extractor->get_user('idnumber', '');
+    }
+
+    /**
+     * Test getting a user by user field.
+     */
+    public function test_get_user_by_user_field() {
+        $this->resetAfterTest();
+
+        $this->fieldsconfig->method('is_field_enabled')->willReturn(true);
+        $this->fieldsconfig->method('is_custom_profile_field')->willReturn(false);
+        $extractor = new user_extractor($this->fieldsconfig);
+
+        $user1 = $this->getDataGenerator()->create_user(['idnumber' => 'user1']);
+        $user2 = $this->getDataGenerator()->create_user(['idnumber' => 'user2']);
+
+        $actual = $extractor->get_user('email', $user1->email);
+        $this->assertSame($user1->id, $actual->id);
+        $this->assertEmpty($actual->profile);
+
+        $actual = $extractor->get_user('id', $user2->id);
+        $this->assertSame($user2->id, $actual->id);
+        $this->assertEmpty($actual->profile);
+
+        $actual = $extractor->get_user('username', 'random user name');
+        $this->assertNull($actual);
+
+        // Test that any of the allowed user fields should be case insensitive.
+        foreach (\tool_token\fields_config::MATCH_FIELDS_FROM_USER_TABLE as $fieldname) {
+            $actual = $extractor->get_user($fieldname, strtoupper($user1->{$fieldname}));
+            $this->assertSame($user1->id, $actual->id);
+        }
+    }
+
+    /**
+     * Test getting a user by custom profile field.
+     */
+    public function test_get_user_by_custom_profile_field() {
+        $this->resetAfterTest();
+
+        $this->fieldsconfig->method('is_field_enabled')->willReturn(true);
+        $this->fieldsconfig->method('is_custom_profile_field')->willReturn(true);
+        $extractor = new user_extractor($this->fieldsconfig);
+
+        $field1 = $this->add_user_profile_field('field1', 'text', true);
+        $field2 = $this->add_user_profile_field('field2', 'text', true);
+
+        $user1 = $this->getDataGenerator()->create_user();
+        profile_save_data((object)['id' => $user1->id, 'profile_field_' . $field1->shortname => 'User 1 Field 1']);
+        profile_save_data((object)['id' => $user1->id, 'profile_field_' . $field2->shortname => 'User 1 Field 2']);
+
+        $user2 = $this->getDataGenerator()->create_user();
+        profile_save_data((object)['id' => $user2->id, 'profile_field_' . $field1->shortname => 'User 2 Field 1']);
+        profile_save_data((object)['id' => $user2->id, 'profile_field_' . $field2->shortname => 'User 2 Field 2']);
+
+        $actual = $extractor->get_user('field1', 'User 1 Field 1');
+        $this->assertSame($user1->id, $actual->id);
+        $this->assertNotEmpty($actual->profile);
+
+        $actual = $extractor->get_user('field2', 'User 2 Field 2');
+        $this->assertSame($user2->id, $actual->id);
+        $this->assertNotEmpty($actual->profile);
+
+        $actual = $extractor->get_user('field1', 'User 1 Field 2');
+        $this->assertNull($actual);
+
+        $actual = $extractor->get_user('random', 'User 1 Field 2');
+        $this->assertNull($actual);
+
+        // Test by custom user profile field should be case sensitive.
+        $actual = $extractor->get_user('field1', 'user 1 field 1');
+        $this->assertNull($actual);
+    }
+
+}
